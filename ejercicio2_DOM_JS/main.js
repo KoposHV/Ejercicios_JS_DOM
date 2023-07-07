1936a2cc03bb7137561e201a9adacdaebb127d89
Kopos <carlosh.viveros12@gmail.com>
2023-07-07 14:13:18 +0200

    ejerciciosjsdom

--- ejercicio2_DOM_JS/main.js
+++ ejercicio2_DOM_JS/main.js
@@ -3,6 +3,17 @@ const myDiv = document.createElement("div");
 document.body.appendChild(myDiv);
 //2.2
 const myDivNew = document.createElement("divNew");
-const myP = document.createElement(myP);
-document.myDivNew.appendChild(myP);
+const p = document.createElement("p");
+document.myDivNew.appendChild(p);
 //2.3
+const div = document.createElement('div');
+for (let i = 1; i <= 6; i++) {
+  const p = document.createElement('p');
+  div.appendChild(p);
+}
+document.body.appendChild(div);
+//2.4
+const myP = document.createElement("myP");
+p.innerHTML = "Soy dinamico";
+//2.5
+

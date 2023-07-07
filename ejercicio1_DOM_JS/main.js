1f6a6e6bb3831c7d33479189e6437ac8d356ecdf
Kopos <carlosh.viveros12@gmail.com>
2023-07-07 11:32:04 +0200

    ejerciciosjsdom

--- ejercicio1_DOM_JS/main.js
+++ ejercicio1_DOM_JS/main.js
@@ -1,2 +1,19 @@
-const myButton = document.querySelector(" .showme");
+//1.1
+const myButton = document.querySelector(".showme");
 console.log(myButton);
+//1.2
+const myH1 = document.querySelector("#pillado");
+console.log(myH1);
+//1.3
+const myP = document.querySelectorAll("AllP");
+console.log(myP);
+//1.4
+const myH4 = document.querySelectorAll(".pokemon");
+console.log(myH4);
+//1.5
+const elementsWithAttribute = document.querySelectorAll('[data-function="testMe"]');
+console.log(elementsWithAttribute);
+//1.6
+const thirdCharacter = document.querySelector('[data-function="testMe"]:nth-of-type(3)');
+console.log(thirdCharacter);
+

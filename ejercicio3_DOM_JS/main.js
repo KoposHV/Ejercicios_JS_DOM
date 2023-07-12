d9b860b963a4ed3b512b97340bfa5051d69e893f
Kopos <carlosh.viveros12@gmail.com>
2023-07-12 08:54:36 +0200

    ejerciciosjsDOM

--- ejercicio3_DOM_JS/main.js
+++ ejercicio3_DOM_JS/main.js
@@ -1,3 +1,4 @@
+//3.1
 const countries = ["Japón", "Nicaragua", "Suiza", "Australia", "Venezuela"];
 const ul = document.createElement("ul");
 for (let i = 0; i < countries.length; i++) {
@@ -6,3 +7,23 @@ for (let i = 0; i < countries.length; i++) {
   ul.appendChild(li);
 }
 document.body.appendChild(ul);
+console.log(ul);
+
+//3.2
+const elementToRemove = document.querySelector(".fn-remove-me");
+elementToRemove.parentNode.removeChild(elementToRemove);
+console.log(elementToRemove);
+
+//3.3
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+const printHereDiv = document.querySelector('div[data-function="printHere"]');
+const ulCars = document.createElement("ul");
+for (const car of cars) {
+const li = document.createElement("li");
+li.innerHTML = car;
+ulCars.appendChild(li);
+console.log(ulCars);
+}
+document.body.appendChild(ulCars);
+
+
